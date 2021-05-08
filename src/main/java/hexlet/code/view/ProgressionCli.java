c918d29bc9a5148a2515f37892fc3b226d8be499
DDBull <dyussupov.d@gmail.com>
2021-05-08 16:52:00 +0500

    Fix ProgressionCli.java

--- src/main/java/hexlet/code/view/ProgressionCli.java
+++ src/main/java/hexlet/code/view/ProgressionCli.java
@@ -8,15 +8,13 @@ public class ProgressionCli extends Cli {
     }
 
     public static void printQuestion(int[] series, int elementToHide, int seriesLength) {
+        System.out.print("Question: ");
+
         for (int i = 0; i < seriesLength; i++) {
             if (series[i] == elementToHide) {
-                System.out.print("..");
+                System.out.print(".. ");
             } else {
-                System.out.printf("%d", series[i]);
-            }
-
-            if (i != seriesLength - 1) {
-                System.out.print(" ");
+                System.out.printf("%d ", series[i]);
             }
         }
         System.out.println();
